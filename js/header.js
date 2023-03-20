71c323573d3b09a6eab1cb51919e5d4477dd9419
Jared Kohrt <jkohrt7@gmail.com>
2023-03-19 23:42:28 -0400

    feat: mobile navbar

--- js/header.js
+++ js/header.js
@@ -1,4 +1,62 @@
-// Provide a variable in css equal to the percentage of the page that's been scrolled through
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+// Collapsable navbar menu for mobile.
+const hamburgerElement = document.querySelectorAll(".hamburger__bar");
+const headerList = document.querySelector(".header__list");
+let isMobile = false;
+let menuOpen = false;
+
+// Hide header contents if window begins in mobile view
+if(window.innerWidth <= 768) {
+    isMobile = true;
+
+    [...hamburgerElement].map((elem) => {
+        elem.setAttribute("hidden", true);
+        headerList.setAttribute("hidden", true);
+    })
+}
+
+// ...and adjust that hidden state whenever window is resized.
+window.addEventListener("resize", () => {
+    if(window.innerWidth <= 768) {
+        isMobile = true;
+        [...hamburgerElement].map((elem) => {
+            elem.setAttribute("hidden", true);
+            headerList.setAttribute("hidden", true);
+        })
+    }
+    else if (isMobile = true) {
+        isMobile = false;
+        [...hamburgerElement].map((elem) => {
+            elem.setAttribute("hidden", false);
+            headerList.setAttribute("hidden", false);
+        })
+    }
+})
+
+document.querySelector(".hamburger").addEventListener("click", ()=> {
+    if(menuOpen) {
+        [...hamburgerElement].map((elem) => {
+            handleMenuClose(elem);
+        });
+        handleMenuClose(headerList);
+    }
+    else {
+        [...hamburgerElement].map((elem) => {
+            handleMenuOpen(elem);
+        });
+        handleMenuOpen(headerList);
+    }
+});
+
+function handleMenuClose(elem) {
+    menuOpen = false;
+    elem.classList.remove("active");
+    elem.setAttribute("hidden", true);
+}
+
+function handleMenuOpen(elem) {
+    menuOpen = true;
+    elem.classList.add("active");
+    elem.setAttribute("hidden", false);
+}
+
+//close menu when anything's clicked
